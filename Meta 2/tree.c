0575d5d302b1f1338810902fcb28ed2f75cc65ff
Joao Pedro Costa Ferreiro <ferreiro@student.dei.uc.pt>
2018-04-03 13:49:12 +0100

    minor change cria no

--- Meta 2/tree.c
+++ Meta 2/tree.c
@@ -6,19 +6,18 @@
 #include <stdlib.h>
 #include "tree.h"
 
-AST cria_no_raiz(tipos tipo_no, char* valor, char* nome_tipo) {
+AST cria_no(tipos tipo_no, char* valor, char* tipo) {
 	AST novo_no = malloc(sizeof(AST));
 
+	novo_no->id_no = 0;
 	novo_no->pai = NULL;
 	novo_no->filho = NULL;
 	novo_no->irmao = NULL;
-	novo_no->id_no = NULL;
-	novo_no->tipo = tipo_no;
 
 	novo_no->valor = (char*)malloc(strlen(valor)*sizeof(char)+1); // +1 para contar com '\0'
-	novo_no->nome_tipo = (char*)malloc(strlen(nome_tipo)*sizeof(char)+1); // +1 para contar com '\0'
+	novo_no->tipo = (char*)malloc(strlen(tipo)*sizeof(char)+1); // +1 para contar com '\0'
 	strcpy(new->valor,valor);
-	strcpy(new->nome_tipo,nome_tipo);
+	strcpy(new->tipo,tipo);
 
 
 	return novo_no;
--- Meta 2/tree.h
+++ Meta 2/tree.h
@@ -5,13 +5,11 @@ Guilherme Cardoso Gomes da Silva 2014226354
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef enum {no_raiz, no_variavel, no_func, no_statement, no_operacao, no_terminal, no_espec} tipos;
 typedef struct AST* no_struct;
 typedef struct AST {
 	int id_no;
 	char* valor;
-	char* nome_tipo;
-	tipos tipo;
+	char* tipo;
 	AST pai;
 	AST filho;
 	AST irmao;
